3639b678d8621bf6fa86c69e42ff5aa394ae9e51
StianPost <69841774+StianPost@users.noreply.github.com>
2021-02-08 15:06:27 +0100

    played around, still not getting it how I want

--- js/script.js
+++ js/script.js
@@ -5,22 +5,31 @@ async function getCoins() {
   const response = await fetch(url);
   const results = await response.json();
   const allCoins = results.data.coins;
-  console.log(allCoins);
+  let count = 0;
+  // console.log(allCoins);
 
   for (let i = 0; i < allCoins.length; i++) {
+    count++;
+    let bGC = 'odd';
+    if (count % 2 === 0) {
+      bGC = 'even';
+    }
+
     main.innerHTML += `
-    <div class="coins ${allCoins[i].symbol}">
-    <img src="${allCoins[i].iconUrl}" height="50px" >
-    <h2>${allCoins[i].name}</h2>
-    <p>${allCoins[i].symbol}</p>
-    <p>Price: ${allCoins[i].price}</p>
-    <p>Ranking: ${allCoins[i].rank}</p>
-    <p>MC: ${allCoins[i].marketCap}</p>
+    <div class="coins ${bGC} ">
+      <img src="${allCoins[i].iconUrl}" height="50px" >
+      <h2>${allCoins[i].name}</h2>
+      <p>${allCoins[i].symbol}</p>
+      <p>Price: ${allCoins[i].price}</p>
+      <p>Ranking: ${allCoins[i].rank}</p>
+      <p>MC: ${allCoins[i].marketCap}</p>
     </div>
     `;
+
+    // coinBG = document.querySelector('.${allCoins[i].symbol}');
     // console.log(allCoins[i]);
     // if (allCoins[i].color) {
-    //   coinBG.style.backgroundColor = allCoins[i].color;
+    //   coinBG.style.backgroundColor = '${allCoins[i].color}';
     // } else {
     //   coinBG.style.backgroundColor = 'white';
     // }
